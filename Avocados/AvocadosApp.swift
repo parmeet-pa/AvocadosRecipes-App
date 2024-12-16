99a38147f081065fb0ffbd28e2a5a17d9d6fe230
parmeet-pa <parmeet.ssaini@gmail.com>
2024-12-16 09:28:46 +0530

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Parmeet Singh on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
